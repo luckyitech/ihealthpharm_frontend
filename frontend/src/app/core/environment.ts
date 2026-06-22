ae6e32ddb42808840dd7793c911e1ec5ca36e2e8
luckyligovi <luvihilucky@gmail.com>
2026-06-22 10:57:56 +0300

    route api calls through iis reverse proxy on port 80 instead of exposing port 6009

--- frontend/src/app/core/environment.ts
+++ frontend/src/app/core/environment.ts
@@ -9,6 +9,7 @@ export class Environment {
 
   // url = 'http://192.168.8.100:6009/ihealthpharmservice/';
   // url = 'http://localhost:6009/ihealthpharmservice/';
-  url = 'http://ipharmke.com:6009/ihealthpharmservice/';
+  // url = 'http://ipharmke.com:6009/ihealthpharmservice/';
+  url = 'http://ipharmke.com/ihealthpharmservice/';
 
 }
